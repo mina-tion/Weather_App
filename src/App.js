91b0852313e1820fc1dd5eee856c327ab0554f48
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2021-11-26 11:26:43 +0200

    refactored

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import { Search } from './Components/Search/Search';
 import { LanguageSettings } from './Components/LanguageSettings/LanguageSettings';
 import { WeatherCard } from './Components/WeatherCard/WeatherCard';
 import { UsePosition } from './Components/Position/UsePosition.js';
-import { useTranslation } from 'react-i18next';
-import { Trans } from 'react-i18next';
 
 function App() {
-	const { t, i18n } = useTranslation();
-
 	return (
 		<div className='App'>
 			<UsePosition />
